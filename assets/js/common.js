a27d69239ee31055f7212c9d6f6e1b72e9124706
jxsn <kome3uma@gmail.com>
2024-08-19 23:34:05 +0900

    20240819_デザイン変更

--- assets/js/common.js
+++ assets/js/common.js
@@ -78,3 +78,27 @@ function changeWord() {
 function pageReload(){
 	window.location.reload();
 }
+
+
+(function () {
+    var copyTarget,copyTargets,num,txt,range;
+    copyTargets = document.querySelectorAll('.copy');
+
+    ClickCopy = function() {
+        copyTarget = this.textContent;
+
+        txt = document.createElement("textarea");
+        txt.value = copyTarget;
+
+        document.body.appendChild(txt);
+        txt.select();
+        document.execCommand('copy');
+        txt.parentElement.removeChild(txt);
+    }
+
+    for(i=0,num=copyTargets.length;i<num;i++) {
+        copyTarget = copyTargets[i];
+        copyTarget.addEventListener('click',ClickCopy);
+    }
+
+}());
