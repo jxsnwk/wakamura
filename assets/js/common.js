12c5836a0ce9d661ad81271cd533290ff8ec993b
jxsn <kome3uma@gmail.com>
2024-11-07 23:07:13 +0900

    20241117_アイコン変更

--- assets/js/common.js
+++ assets/js/common.js
@@ -80,25 +80,25 @@ function pageReload(){
 }
 
 
-(function () {
+function clickCopyText() {
     var copyTarget,copyTargets,num,txt,range;
     copyTargets = document.querySelectorAll('.copy');
 
-    ClickCopy = function() {
-        copyTarget = this.textContent;
+	var ClickCopy = function() {
+		copyTarget = this.textContent;
+	
+		txt = document.createElement("textarea");
+		txt.value = copyTarget;
+	
+		document.body.appendChild(txt);
+		txt.select();
+		document.execCommand('copy');
+		txt.parentElement.removeChild(txt);
+	}
 
-        txt = document.createElement("textarea");
-        txt.value = copyTarget;
-
-        document.body.appendChild(txt);
-        txt.select();
-        document.execCommand('copy');
-        txt.parentElement.removeChild(txt);
-    }
-
-    for(i=0,num=copyTargets.length;i<num;i++) {
+	for(i=0,num=copyTargets.length;i<num;i++) {
         copyTarget = copyTargets[i];
         copyTarget.addEventListener('click',ClickCopy);
     }
 
-}());
+};
